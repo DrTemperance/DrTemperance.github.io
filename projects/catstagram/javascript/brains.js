4287de92536cfa18f2fa0bef9b5eabffc22acb15
Tεmρεrαηcε <125318692+DrTemperance@users.noreply.github.com>
2023-06-16 15:55:13 -0500

    catty grams

---
+++ projects/catstagram/javascript/brains.js
@@ -0,0 +1,55 @@
+var likes1 = 0;
+
+function likePhoto1() {
+  var likeCount1 = document.getElementById("likeCount1");
+  if (likes1 != 1) {
+    likes1 += 1;
+  } else {
+    likes1 -= 1;
+  }
+  likeCount1.innerHTML = likes1;
+}
+var likes2 = 0;
+
+function likePhoto2() {
+  var likeCount2 = document.getElementById("likeCount2");
+  if (likes2 != 1) {
+    likes2 += 1;
+  } else {
+    likes2 -= 1;
+  }
+  likeCount2.innerHTML = likes2;
+}
+var likes3 = 0;
+
+function likePhoto3() {
+  var likeCount3 = document.getElementById("likeCount3");
+  if (likes3 != 1) {
+    likes3 += 1;
+  } else {
+    likes3 -= 1;
+  }
+  likeCount3.innerHTML = likes3;
+}
+var likes4 = 0;
+
+function likePhoto4() {
+  var likeCount4 = document.getElementById("likeCount4");
+  if (likes4 != 1) {
+    likes4 += 1;
+  } else {
+    likes4 -= 1;
+  }
+  likeCount4.innerHTML = likes4;
+}
+var likes5 = 0;
+
+function likePhoto5() {
+  var likeCount5 = document.getElementById("likeCount5");
+  if (likes5 != 1) {
+    likes5 += 1;
+  } else {
+    likes5 -= 1;
+  }
+  likeCount5.innerHTML = likes5;
+}
