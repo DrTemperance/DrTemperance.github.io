6dee30eb65c8f9534c523f768bb7187faa719fc2
Tεmρεrαηcε <125318692+DrTemperance@users.noreply.github.com>
2023-08-20 08:18:11 -0500

    Update NOINSPECTING.js

--- meaty/NOINSPECTING.js
+++ meaty/NOINSPECTING.js
@@ -1,10 +1,1 @@
-$(document).keydown(function (event) {
-  if (event.keyCode == 123) {
-    return false;
-  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-    return false;
-  }
-});
-$(document).on("contextmenu", function (e) {
-  e.preventDefault();
-});
+$(document).keydown(function(e){return 123!=e.keyCode&&(!e.ctrlKey||!e.shiftKey||73!=e.keyCode)&&void 0}),$(document).on("contextmenu",function(e){e.preventDefault()});
