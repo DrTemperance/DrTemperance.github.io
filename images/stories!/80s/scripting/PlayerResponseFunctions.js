ce651effeeb1a75a5b5d24c792d40cb3c33a623a
drtemperance <jellow2323@gmail.com>
2023-08-23 20:46:30 -0500

    papua pigs

--- images/stories!/80s/scripting/PlayerResponseFunctions.js
+++ images/stories!/80s/scripting/PlayerResponseFunctions.js
@@ -1,32 +1,27 @@
 function playerScriptsHandler(lowerInput) {
-  if (lowerInput.includes("go")) {
-    var index = lowerInput.indexOf(" to");
-    if (index != -1) {
-      var functionName = lowerInput.substring(0, index);
-      var argument = lowerInput.substring(index + 4).replace(/ /g, "");
-      return `${functionName}('${argument}')`;
-    } else {
-      return `${lowerInput}`;
-    }
-  } else if (lowerInput.includes("inspect")) {
-    var index = lowerInput.indexOf(" ");
-    if (index != -1) {
-      var functionName = lowerInput.substring(0, index);
-      var argument = lowerInput.substring(index).replace(/ /g, "");
-      return `${functionName}(${argument})`;
-    } else {
-      return `${lowerInput}`;
-    }
-  } else if (lowerInput.includes("enter")) {
-    var index = lowerInput.indexOf(" ");
-    if (index != -1) {
-      var functionName = lowerInput.substring(0, index);
-      var argument = lowerInput.substring(index).replace(/ /g, "");
-      return `${functionName}(${argument})`;
-    } else {
-      return `${lowerInput}`;
-    }
-  } else {
-    return `${lowerInput}`;
-  }
+   let index, argument, functionName;
+   switch (true) {
+      case lowerInput.includes('go'):
+         index = lowerInput.indexOf(' to');
+         if (index === -1) {
+            return `${lowerInput}`;
+         } else {
+            functionName = lowerInput.substring(0, index);
+            argument = lowerInput.substring(index + 4).replace(/ /g, '');
+            return `${functionName}('${argument}')`;
+         }
+         break;
+      case lowerInput.includes('inspect') || lowerInput.includes('enter'):
+         index = lowerInput.indexOf(' ');
+         if (index === -1) {
+            return `${lowerInput}`;
+         } else {
+            functionName = lowerInput.substring(0, index);
+            argument = lowerInput.substring(index).replace(/ /g, '');
+            return `${functionName}(${argument})`;
+         }
+         break;
+      default:
+         return `${lowerInput}`;
+   }
 }
--- images/stories!/80s/scripting/data.js
+++ images/stories!/80s/scripting/data.js
@@ -1,76 +1,81 @@
 const locations = {
-  gasstation: [
-    {
-      locale: "home",
-      timeCost: 33,
-      outputname: "your house",
-      requirements: { hasKeys: true },
-    },
-  ],
-  home: [
-    {
-      locale: "gasstation",
-      timeCost: 33,
-      outputname: "the gas station",
-      requirements: { onHomeArrest: false },
-    },
-  ],
-};
+         gasstation: [
+            {
+               locale      : 'home',
+               outputname  : 'your house',
+               requirements: {hasKeys: true},
+            },
+            {
+               locale      : 'motel',
+               outputname  : 'the motel',
+               requirements: {clearedMotel: true},
+            },
+         ],
+         home      : [
+            {
+               locale      : 'gasstation',
+               outputname  : 'the gas station',
+               requirements: {onHomeArrest: false},
+            },
+         ],
+         motel     : [
+            {
+               locale      : 'home',
+               outputname  : 'your house',
+               requirements: {hasKeys: true},
+            },
+            {
+               locale      : 'gasstation',
+               outputname  : 'the gas station',
+               requirements: {onHomeArrest: false},
+            },
+         ],
+      }
+;
 
-function checkItem(item) {
-  return inventory.items[item].available;
-}
-
-function checkCondition(item) {
-  return inventory.items[item].d;
-}
-
-function checkValue(value) {
-  return inventory.values[value];
-}
-
-function checkKeys(key) {
-  return inventory.keys[key].available;
-}
+const checkItem = item => inventory.items[item].available;
+const checkCondition = item => inventory.items[item].d;
+const checkValue = value => inventory.values[value];
+const checkKeys = key => inventory.keys[key].available;
 
 var inventory = {
-  values: { money: 3, hairpins: 1 },
-  items: {
-    flashlight: {
-      available: false,
-      condition: { type: "battery", value: 100 },
-    },
-    taser: {
-      available: true,
-      condition: {
-        type: "battery",
-        value: 0,
+   values: {money: 3, hairpins: 1},
+   items : {
+      flashlight    : {
+         available: false,
+         condition: {type: 'battery', value: 100},
       },
-    },
-    lighter: {
-      available: true,
-      condition: {
-        type: "fuel",
-        value: 80,
+      taser         : {
+         available: true,
+         condition: {
+            type : 'battery',
+            value: 0,
+         },
       },
-    },
-    sharpBone: {
-      available: false,
-    },
-    looseCigarette: {
-      available: false,
-    },
-    revolver: {
-      available: false,
-      stashed: false,
-      condition: {
-        type: "ammo",
-        value: 0,
+      lighter       : {
+         available: true,
+         condition: {
+            type : 'fuel',
+            value: 80,
+         },
       },
-    },
-  },
-  keys: {
-    motelMasterKey: { available: false },
-    carKeys: { available: true },
-  },
+      sharpBone     : {
+         available: false,
+      },
+      looseCigarette: {
+         available: false,
+      },
+      revolver      : {
+         available: false,
+         stashed  : false,
+         condition: {
+            type : 'ammo',
+            value: 0,
+         },
+      },
+   },
+   keys  : {
+      motelMasterKey: {available: false},
+      carKeys       : {available: true},
+   },
 };
