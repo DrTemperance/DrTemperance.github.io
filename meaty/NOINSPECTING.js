857512d50ca36c59d54e715152244f72b25a6ca3
drtemperance <jellow2323@gmail.com>
2023-08-24 21:04:16 -0500

    papua pigs 2!!!!!
    lots!fwqeas PLEASE LET ME LIVE

--- meaty/NOINSPECTING.js
+++
@@ -1,9 +0,0 @@
-function handleKeyDownEvent(event) {
-    const forbiddenKeyCodes = new Set(["F12"]);
-    if ((event.ctrlKey && event.shiftKey && event.key === "i") || forbiddenKeyCodes.has(event.key)) {
-        event.preventDefault();
-    }
-}
-
-document.addEventListener("keydown", handleKeyDownEvent, false);
-document.oncontextmenu = e => e.preventDefault();
