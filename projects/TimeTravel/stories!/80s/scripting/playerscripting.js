3ff6058a0e431a0fb8d5df313aebbe835c9fb622
temperance <jellow2323@gmail.com>
2023-10-09 19:50:11 -0500

    updadda

--- projects/TimeTravel/stories!/80s/scripting/playerscripting.js
+++ projects/TimeTravel/stories!/80s/scripting/playerscripting.js
@@ -2,18 +2,8 @@ let from = 'home';
 
 function go(to) {
   let availLocations = locations[from];
-  let index = availLocations.findIndex(
-     possibility => possibility.locale===to,
-  );
-  if (index!== -1) {
-    return `You walk to ${to.outputname}.`;
-  } else if (to===``) {
-    return `You ain't going nowhere.`;
-  } else {
-    if (from===to) {
-      return `You are already there.`;
-    } else {
-      return `It is not possible to go there from your location.`;
-    }
+  let index = availLocations.findIndex(possibility => possibility.locale===to);
+  if (index!== -1) {return `You walk to ${to.outputname}.`;} else if (to===``) {return `You ain't going nowhere.`;} else {
+    if (from===to) {return `You are already there.`;} else {return `It is not possible to go there from your location.`;}
   }
 }
