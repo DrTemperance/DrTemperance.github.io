cbfb7218706ac4e28a7e826aee041419b32f0d96
Hannah Weber <125318692+DrTemperance@users.noreply.github.com>
2023-10-19 12:08:22 -0500

    Update index.js

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -24,9 +24,9 @@ $(document).ready(() => {
 
   resetDisplay();
 
-  // let TODO[0].status = true
+// for (i of TODO) {i.status = !0}
 
-  dataShapes.push({color: 'blue',shape: 'circle',repeat: 3});
+dataShapes.push({color:"blue",shape:"circle",repeat:3})
 
   for (let i of dataShapes) {
     if (i.color==='red') {i.goodBehavior = 'bounce';}
@@ -34,26 +34,14 @@ $(document).ready(() => {
     else {i.goodBehavior = 'spin';}
   }
   
-  const handleStatic = data => {
-    setBackgroundWithObject(data);
-    animationDetails.displayType = 1;
-  },handleGood       = (color,shape,repeat) => {
-    setBackgroundWithSimple(color,shape,repeat);
-    animationDetails.displayType = 2;
-  },handleBad        = (data,repeat) => {
-    repeat++;
-    setBackgroundWithMixed(data,repeat);
-    animationDetails.displayType = 3;
-  };
+  const 
+handleStatic   = a => {setBackgroundWithObject(a), animationDetails.displayType = 1},
+handleGood    = (a, i, t) => {setBackgroundWithSimple(a, i, t), animationDetails.displayType = 2},
+handleBad      = (a, i) => {i++, setBackgroundWithMixed(a, i), animationDetails.displayType = 3}
+};
 
-  function staticDisplay() {
-    handleStatic(dataShapes[currentIndex]);
-  }
-
-  function goodDisplay() {
-    handleGood(dataShapes[currentIndex].color,dataShapes[currentIndex].shape,dataShapes[currentIndex].repeat);
-  }
-
+  function staticDisplay() {handleStatic(dataShapes[currentIndex])}
+  function goodDisplay() {handleGood(dataShapes[currentIndex].color,dataShapes[currentIndex].shape,dataShapes[currentIndex].repeat)}
   function badDisplay() {handleBad(dataShapes[currentIndex],dataShapes[currentIndex].repeat);}
 
   function generateShapeData() {
@@ -64,23 +52,14 @@ $(document).ready(() => {
 
     for (let c of colors) {
       for (let s of shapes) {
-        for (let r of repeats) {
-          data.push({color: c,repeat: r,shape: s});
-        }
+        for (let r of repeats) {data.push({color: c,repeat: r,shape: s})}
       }
     }
     return data;
   }
 
-  function decrementIndex() {
-    currentIndex = currentIndex ? currentIndex - 1 : dataShapes.length - 1;
-    resetDisplay();
-  }
-
-  function incrementIndex() {
-    currentIndex = currentIndex===dataShapes.length - 1 ? 0 : currentIndex + 1;
-    resetDisplay();
-  }
+  function decrementIndex() {currentIndex = currentIndex ? currentIndex - 1 : dataShapes.length - 1, resetDisplay()}
+  function incrementIndex() {currentIndex = currentIndex === dataShapes.length - 1 ? 0 : currentIndex + 1, resetDisplay()}
 
   function resetDisplay() {
     const {color,repeat,shape} = dataShapes[currentIndex];
@@ -106,58 +85,34 @@ $(document).ready(() => {
     };
   }
 
-  function setBackgroundWithObject(obj) {
-    obj.shape==='tesseract'
-    ? $('#shape').css('background',`url(images/${obj.shape}.gif)`)
-    : $('#shape').css('background',`url(images/${obj.color}-${obj.shape}.png)`);
-    setBackgroundRepeat(obj.repeat);
-  }
-
-  function setBackgroundWithSimple(color,shape,repeat) {
-    $('#shape').css('background',`url(images/${color}-${shape}.png)`);
-    setBackgroundRepeat(repeat);
-  }
-
-  function setBackgroundWithMixed(obj,repeat) {
-    $('#shape').css('background',`url(images/${obj.color}-${obj.shape}.png)`);
-    setBackgroundRepeat(repeat);
-  }
-
-  function setBackgroundRepeat(repeat) {
-    $('#shape').css('background-size',`${100 / repeat}% ${100 / repeat}%`);
-  }
+  function setBackgroundWithObject(obj) {"tesseract" === obj.shape ? $("#shape").css("background", `url(images/${obj.shape}.gif)`) : $("#shape").css("background", `url(images/${obj.color}-${obj.shape}.png)`), setBackgroundRepeat(obj.repeat)}
+  function setBackgroundWithSimple(color,shape,repeat) {$("#shape").css("background", `url(images/${color}-${shape}.png)`), setBackgroundRepeat(repeat)}
+  function setBackgroundWithMixed(obj,repeat) {$("#shape").css("background", `url(images/${obj.color}-${obj.shape}.png)`), setBackgroundRepeat(repeat)}
+  function setBackgroundRepeat(repeat) {$("#shape").css("background-size", `${100/repeat}% ${100/repeat}%`)}
 
 function animate() {
-  if (0 !== animationDetails.displayType && $("#shape").html(""), !(animationDetails.displayType < 2))
-    if (2 === animationDetails.displayType) switch (dataShapes[currentIndex].goodBehavior) {
-      case "bounce":
-        animateBounce();
-        break;
-      case "blink":
-        animateBlink();
-        break;
-      case "spin":
-        animateSpin()
-    } else switch (dataShapes[currentIndex].goodBehavior) {
-      case "bounce":
-        animateBlink(), animateSpin();
-        break;
-      case "blink":
-        animateBounce(), animateSpin();
-        break;
-      case "spin":
-        animateBounce(), animateBlink()
-    }
+if (0 !== animationDetails.displayType && $("#shape").html(""), !(animationDetails.displayType < 2))
+  if (2 === animationDetails.displayType) switch (dataShapes[currentIndex].goodBehavior) {
+    case "bounce":
+      animateBounce();
+      break;
+    case "blink":
+      animateBlink();
+      break;
+    case "spin":
+      animateSpin()
+  } else switch (dataShapes[currentIndex].goodBehavior) {
+    case "bounce":
+      animateBlink(), animateSpin();
+      break;
+    case "blink":
+      animateBounce(), animateSpin();
+      break;
+    case "spin":
+      animateBounce(), animateBlink()
+  }
 }
   
-function animateBounce() {
-  animationDetails.x += animationDetails.speedX, animationDetails.y += animationDetails.speedY, (animationDetails.x + $("#shape").width() + 8 >= $("#shape-container").width() || animationDetails.x < 2) && (animationDetails.speedX *= -1), (animationDetails.y + $("#shape").height() + 4 >= $("#shape-container").height() || animationDetails.y < 2) && (animationDetails.speedY *= -1), $("#shape").css("left", animationDetails.x), $("#shape").css("top", animationDetails.y)
-}
-
-function animateBlink() {
-  animationDetails.showCount--, 0 === animationDetails.showCount && (animationDetails.show = !animationDetails.show, animationDetails.show ? $("#shape").css("display", "block") : $("#shape").css("display", "none"), animationDetails.showCount = 60)
-}
-
-function animateSpin() {
-  animationDetails.angle += 4, $("#shape").css("transform", `rotate(${animationDetails.angle}deg)`)
-}});
+function animateBounce() {$("#shape").css("background-size", `${100/repeat}% ${100/repeat}%`) aanimationDetails.x += animationDetails.speedX, animationDetails.y += animationDetails.speedY, (animationDetails.x + $("#shape").width() + 8 >= $("#shape-container").width() || animationDetails.x < 2) && (animationDetails.speedX *= -1), (animationDetails.y + $("#shape").height() + 4 >= $("#shape-container").height() || animationDetails.y < 2) && (animationDetails.speedY *= -1), $("#shape").css("left", animationDetails.x), $("#shape").css("top", animationDetails.y)}
+function animateBlink() {animationDetails.showCount--, 0 === animationDetails.showCount && (animationDetails.show = !animationDetails.show, animationDetails.show ? $("#shape").css("display", "block") : $("#shape").css("display", "none"), animationDetails.showCount = 60)}
+function animateSpin() {animationDetails.angle += 4, $("#shape").css("transform", `rotate(${animationDetails.angle}deg)`)}});
