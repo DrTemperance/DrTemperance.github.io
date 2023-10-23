5e4b55112991fe72b9e58d2c2a07eba43dcc0468
temperance <jellow2323@gmail.com>
2023-10-23 11:58:43 -0500

    megamind v2.0

--- projects/PlatformerRework/js/factory/platform.js
+++ projects/PlatformerRework/js/factory/platform.js
@@ -1,19 +1,18 @@
 (function (window) {
-    'use strict';
-    window.opspark = window.opspark || {};
-    let opspark = window.opspark;
-    window.opspark.platform = window.opspark.platform || {};
-    
-    opspark.platform.factory = function (game) {
-        game.platforms = game.add.group();
-        game.platforms.enableBody = true;
-        
-        function create(x, y, scaleX, scaleY, immovable) {
-            var platform = game.platforms.create(x, y, 'platform');
-            platform.scale.setTo(scaleX || 1, scaleY || 1);
-            platform.body.immovable = immovable || true;
-            return platform;
-        }
-        opspark.platform.create = create;
-    };
+  'use strict';
+  window.opspark = window.opspark || {};
+  let opspark = window.opspark;
+  window.opspark.platform = window.opspark.platform || {};
+
+  opspark.platform.factory = game => {
+	 game.platforms = game.add.group();
+	 game.platforms.enableBody = !0;
+
+	 opspark.platform.create = (x,y,scaleX,scaleY,immovable) => {
+		let platform = game.platforms.create(x,y,'platform');
+		platform.scale.setTo(scaleX || 1,scaleY || 1);
+		platform.body.immovable = immovable || !0;
+		return platform;
+	 };
+  };
 })(window);
