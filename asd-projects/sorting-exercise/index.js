a70a332a4f2a6a3b30edebdffc719007873ea160
DrTemperance <jellow2323@gmail.com>
2023-10-24 05:30:37 -0500

    sorting finished and optimized

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -1,94 +1,47 @@
-/* IMPORTANT VALUES
-
-This section contains a list of all variables predefined for you to use (that you will need)
-
-The CSS ids you will work with are:
-
-1. bubbleCounter -- the container for the counter text for bubble sort
-2. quickCounter  -- the container for the counter text for quick sort
-
-*/
-
-///////////////////////////////////////////////////////////////////////
-/////////////////////// YOUR WORK GOES BELOW HERE /////////////////////
-///////////////////////////////////////////////////////////////////////
-
-// TODO 2: Implement bubbleSort
 async function bubbleSort(array) {
-    for (let i = 0; i < array.length; i++) {
-        for (let j = array.length - 1; j >= i + 1; j--) {
-            if (array[j].value < array[j - 1].value) {
-                swap(array, j, j - 1)
-                updateCounter(bubbleCounter);
-                await sleep();
-            }
-        }
-    }
+  for (let i = 0; i<array.length; i++) {
+	 for (let a = array.length - 1; a>=i + 1; a--) {
+		array[a].value<array[a - 1].value && (swap(array,a,a - 1), updateCounter(bubbleCounter), await sleep());
+	 }
+  }
 }
 
-// TODO 3: Implement quickSort
-async function quickSort(array, left, right) {
-    if (right - left > 0) {
-        var index = await partition(array, left, right)
-        if (left < index - 1) {
-            quickSort(array, left, index - 1)
-        }
-        if (index < right) {
-            quickSort(array, index, right)
-        }
-    }
+async function quickSort(array,left,right) {
+  if (right - left>0) {
+	 let t = await partition(array,left,right);
+	 t - 1>left && quickSort(array,left,t - 1);
+	 t<right && quickSort(array,t,right);
+  }
 }
 
-// TODOs 4 & 5: Implement partition
-async function partition(array, left, right) {
-    var pivot = array[Math.floor((right + left)/2)].value;
-    while (left < right) {
-        while (array[left].value < pivot) {
-            left++
-        }
-        while (array[right].value > pivot) {
-            right--
-        }
-        if (left < right) {
-            swap(array, left, right)
-            updateCounter(quickCounter)
-            await sleep()
-        }
-    }
-    return left + 1
+async function partition(array,left,right) {
+  let pivot = array[Math.floor((right + left) / 2)].value;
+  while (left<right) {
+	 while (array[left].value<pivot) {left++}
+	 while (array[right].value>pivot) {right--}
+	 if (left<right) {
+		swap(array,left,right);
+		updateCounter(quickCounter);
+		await sleep()
+	 }
+  }
+  return left + 1
 }
 
-// TODO 1: Implement swap
-function swap(array, i, j) {
-    var temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
-    drawSwap(array, i, j)
+async function swap(array,i,j) {
+  let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+  drawSwap(array,i,j)
 }
 
-///////////////////////////////////////////////////////////////////////
-/////////////////////// YOUR WORK GOES ABOVE HERE /////////////////////
-///////////////////////////////////////////////////////////////////////
+async function sleep() {return new Promise(resolve => setTimeout(resolve,50));}
 
-//////////////////////////// HELPER FUNCTIONS /////////////////////////
+async function drawSwap(array,i,j) {
+  let {id: id1} = array[i],{id: id2} = array[j],temp = `${parseFloat($(id1).css('top'))}px`;
 
-// this function makes the program pause by SLEEP_AMOUNT milliseconds whenever it is called
-function sleep(){
-    return new Promise(resolve => setTimeout(resolve, SLEEP_AMOUNT));
+  $(id1).css('top',`${parseFloat($(id2).css('top'))}px`);
+  $(id2).css('top',temp);
 }
 
-// This function draws the swap on the screen
-function drawSwap(array, i, j){
-    let element1 = array[i];
-    let element2 = array[j];
-
-    let temp = parseFloat($(element1.id).css("top")) + "px";
-
-    $(element1.id).css("top", parseFloat($(element2.id).css("top")) + "px");
-    $(element2.id).css("top", temp);
-}
-
-// This function updates the specified counter
-function updateCounter(counter){
-    $(counter).text("Move Count: " + (parseFloat($(counter).text().replace(/^\D+/g, '')) + 1));
-}
+async function updateCounter(counter) {$(counter).text(`Move Count: ${parseFloat($(counter).text().replace(/^\D+/g,'')) + 1}`)}
--- asd-projects/sorting-exercise/run.js
+++ asd-projects/sorting-exercise/run.js
@@ -1,18 +1,7 @@
-// flag to prevent running simultaneous sorts by clicking 
-// "start" multiple times
-let STARTED = false;
+let STARTED = !1;
 
-$(document).ready(function(){
-    $("#goButton").on("click", function(){
-        if (!STARTED){
-            STARTED = true;
-
-            if (bubbleSort){
-                bubbleSort(bubbleList);
-            }
-            if (quickSort){
-                quickSort(quickList, 0, quickList.length-1);
-            }
-        }
-    })
-})
+$(document).ready(() => {
+  $('#goButton').on('click',() => {
+	 STARTED || (STARTED = !0, bubbleSort && bubbleSort(bubbleList), quickSort && quickSort(quickList,0,quickList.length - 1));
+  })
+});
