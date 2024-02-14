7513ea32b6e132375346b0aff93c4d66bbb97c7a
jello <jellow2323@gmail.com>
2024-02-14 06:54:10 -0600

    BIG OMG 3D MODEL THING? HELP

--- Projects/TEST/main.js
+++ Projects/TEST/main.js
@@ -1,111 +1,95 @@
+// ~ Variable Sector ~ //
 import * as THREE from "./three/build/three.module.js";
 
-const Scene             = new THREE.Scene(),
-      perspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000),
-      Renderer          = new THREE.WebGLRenderer({ antialias: true, canvas: document.getElementById("canvas") }),
-      Mesh              = new THREE.Mesh(
+const Scene    = new THREE.Scene(),
+      Viewport = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000),
+      Renderer = new THREE.WebGLRenderer({ antialias: true, canvas: document.getElementById("canvas") }),
+      Lighting = new THREE.HemisphereLight(0xAA55AA, 0xAAAAAA, 0.9),
+      Mesh     = new THREE.Mesh(
 	       new THREE.BoxGeometry(3.5, 3.5, 3.5),
 	       new THREE.MeshLambertMaterial({ color: 0xFF6050 })
       );
 
-let Animating = false,
-    AnimStart = null,
-    Pre_Rotation;
+Mesh.castShadow = Mesh.receiveShadow = true;
 
+
+// Initialization //
 Renderer.setSize(window.innerWidth, window.innerHeight);
 
+Lighting.position.set(0, 5, 0);
+Viewport.position.set(1, 0, 5);
 
-document.body.appendChild(Renderer.domElement);
+Scene.add(Lighting).add(Mesh);
 
-const StartScene = async () => {
-	let SceneLighting = await new THREE.HemisphereLight(0xAA55AA);
-	Scene.add(SceneLighting)
-	     .add(Mesh);
-	SceneLighting.position.set(0, 5, 0);
+Renderer.setClearColor(0x111111);
 
-	perspectiveCamera.position.set(1, 0, 5);
-	Mesh.castShadow = true;
-	Mesh.receiveShadow = true;
+// Render Loop //
+setInterval(() => {
+	Viewport.updateProjectionMatrix();
+	Viewport.lookAt(Mesh.position);
+	Renderer.render(Scene, Viewport);
+});
 
-	Renderer.setClearColor(0x111111);
-};
+// ~ Animation Sector ~ //
 
-const animate = async () => {
-	perspectiveCamera.updateProjectionMatrix();
-	requestAnimationFrame(animate);
-
-	perspectiveCamera.lookAt(Mesh.position);
-
-
-	Renderer.render(Scene, perspectiveCamera);
-};
-
-animate();
-StartScene();
+let Animating = false,
+    AnimStart = null,
+    AnimProgress,
+    Pre_Rotation;
 
 document.addEventListener('keydown', async ({ key }) => {
-	if (!Animating) {
+	if (!Animating && (key==='w' || key==='a' || key==='s' || key==='d')) {
+		AnimStart = null;
+		Animating = true;
 		switch (key) {
 			case 'w':
-				Animating = true;
-				AnimStart = null;
 				Pre_Rotation = Mesh.rotation.x;
 
 				const AnimateUp = async time => {
 					if (AnimStart===null) AnimStart = time;
-					const progress = (time - AnimStart) / 500;
-					Mesh.rotation.x = Pre_Rotation + (Math.PI / 2 - Pre_Rotation) * progress;
+					AnimProgress = (time - AnimStart) / 500;
+					Mesh.rotation.x = Pre_Rotation + (Math.PI / 2 - Pre_Rotation) * AnimProgress;
 
-					if (progress<1) requestAnimationFrame(AnimateUp);
-					else Mesh.rotation.x = Math.PI / 2, Animating = false;
+					AnimProgress<1 ? requestAnimationFrame(AnimateUp) : (Mesh.rotation.x = Math.PI / 2, Animating = false);
 				};
 
 				requestAnimationFrame(AnimateUp);
 				break;
 			case 's':
-				Animating = true;
-				AnimStart = null;
 				Pre_Rotation = Mesh.rotation.x;
 
 				const AnimateDown = async time => {
 					if (AnimStart===null) AnimStart = time;
-					const progress = (time - AnimStart) / 500;
-					Mesh.rotation.x = Pre_Rotation - (Pre_Rotation) * progress;
+					AnimProgress = (time - AnimStart) / 500;
+					Mesh.rotation.x = Pre_Rotation - (Pre_Rotation) * AnimProgress;
 
-					if (progress<1) requestAnimationFrame(AnimateDown);
-					else Mesh.rotation.x = 0, Animating = false;
+					AnimProgress<1 ? requestAnimationFrame(AnimateDown) : (Mesh.rotation.x = 0, Animating = false);
 				};
 
 				requestAnimationFrame(AnimateDown);
 				break;
 			case 'a' :
-				Animating = true;
-				AnimStart = null;
 				Pre_Rotation = Mesh.rotation.y;
 
 				const AnimateWest = async time => {
 					if (AnimStart===null) AnimStart = time;
-					const progress = (time - AnimStart) / 500;
-					Mesh.rotation.y = Pre_Rotation + (Math.PI / 2 - Pre_Rotation) * progress;
+					AnimProgress = (time - AnimStart) / 500;
+					Mesh.rotation.y = Pre_Rotation + (Math.PI / 2 - Pre_Rotation) * AnimProgress;
 
-					if (progress<1) requestAnimationFrame(AnimateWest);
-					else Mesh.rotation.y = Math.PI / 2, Animating = false;
+					AnimProgress<1 ? requestAnimationFrame(AnimateWest) : (Mesh.rotation.y = Math.PI / 2, Animating = false);
 				};
 
 				requestAnimationFrame(AnimateWest);
 				break;
 			case 'd' :
-				Animating = true;
-				AnimStart = null;
 				Pre_Rotation = Mesh.rotation.y;
 
 				const AnimateEast = async time => {
 					if (AnimStart===null) AnimStart = time;
-					const progress = (time - AnimStart) / 500;
-					Mesh.rotation.y = Pre_Rotation - (Pre_Rotation) * progress;
+					AnimProgress = (time - AnimStart) / 500;
+					Mesh.rotation.y = Pre_Rotation - (Pre_Rotation) * AnimProgress;
 
-					if (progress<1) requestAnimationFrame(AnimateEast);
-					else Mesh.rotation.y = 0, Animating = false;
+					AnimProgress<1 ? requestAnimationFrame(AnimateEast) : (Mesh.rotation.y = 0, Animating = false);
 				};
 
 				requestAnimationFrame(AnimateEast);
