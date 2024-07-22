dbb42f6a7488db6d4a0384af3767927f03aee0a8
temperance <jellow2323@gmail.com>
2024-07-22 14:53:27 -0500

    mama mia

--- Miss Index/metacrisis.js
+++
@@ -1,7 +0,0 @@
-const metacrisis = 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1;=>{
-	while (!0) {
-		document.appendChild(document.createTextNode('Binary'))
-	}
-
-	setInterval(window.open('https://www.google.com/search?q=binary'), 500);
-}
